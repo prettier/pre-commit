b1c32a9068306a0ee5950c39b68149182d5808e2
fisker <lionkay@gmail.com>
2020-08-07 09:14:43 +0800

    Test

---
+++ 1.js
@@ -0,0 +1,15 @@
+#!/usr/bin/env node
+
+"use strict";
+
+// const path = require("path");
+// const prettierPackageJson = require.resolve("prettier/package.json");
+// const { bin } = require(prettierPackageJson);
+// const directory = path.dirname(prettierPackageJson);
+// const binFile = path.join(
+//   directory,
+//   typeof bin === "string" ? bin : bin.prettier
+// );
+
+console.log ({process})
+
