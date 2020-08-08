d781e90b06ea19d4f3dfb90e7ce8ffe100779623
fisker <lionkay@gmail.com>
2020-08-08 21:51:35 +0800

    Add create script

---
+++ scripts/build-version.js
@@ -0,0 +1,104 @@
+"use strict";
+
+const fs = require("fs");
+const path = require("path");
+const execa = require("execa");
+const fetch = require("node-fetch");
+const Listr = require("listr");
+const prompt = require("enquirer").prompt;
+const yaml = require("js-yaml");
+const prettier = require("prettier");
+
+const ROOT = path.join(__dirname, "..");
+
+async function getVersions() {
+  const response = await fetch("https://registry.npmjs.org/prettier");
+  const json = await response.json();
+  return Object.keys(json.versions).reverse();
+}
+
+async function selectVersions(versions) {
+  const answers = await prompt({
+    type: "multiselect",
+    message: "Select version(s) you want create:",
+    choices: versions,
+    initial: versions,
+    limit: 15,
+  });
+
+  return Array.isArray(answers) ? answers : [answers];
+}
+
+let hooks;
+const hooksFile = path.join(ROOT, ".pre-commit-hooks.yaml");
+function updateFiles(version) {
+  hooks = hooks || fs.readFileSync(hooksFile, "utf8");
+  fs.writeFileSync(
+    hooksFile,
+    hooks.replace(/prettier@.*\n/, `prettier@${version}`)
+  );
+}
+
+(async () => {
+  const allVersions = await getVersions();
+  let versions = process.argv.slice(2);
+
+  for (const version of versions) {
+    if (!allVersions.includes(version)) {
+      throw new Error(`Version ${version} is not found.`);
+    }
+  }
+
+  if (versions.length === 0) {
+    versions = await selectVersions(allVersions);
+  }
+
+  const tasks = new Listr(
+    versions.map((version) => {
+      const prefixedVersion = `v${version}`;
+      return {
+        title: prefixedVersion,
+        task: () =>
+          new Listr([
+            // {
+            //   title: `Switch to "main" branch`,
+            //   task: () => execa("git", ["checkout", "main"]),
+            // },
+            {
+              title: `Delete "${prefixedVersion}" branch`,
+              task: () => execa("git", ["branch", "-D", prefixedVersion]).catch(() => {}),
+            },
+            {
+              title: `Delete "${prefixedVersion}" tag`,
+              task: () => execa("git", ["tag", "-d", prefixedVersion]).catch(() => {}),
+            },
+            {
+              title: `Create "${prefixedVersion}" branch`,
+              task: () => execa("git", ["branch", prefixedVersion]),
+            },
+            {
+              title: `Switch to "${prefixedVersion}" branch`,
+              task: () => execa("git", ["checkout", prefixedVersion]),
+            },
+            {
+              title: `Update files`,
+              task: () => updateFiles(version),
+            },
+            {
+              title: `Create "${prefixedVersion}" tag`,
+              task: () =>
+                execa("git", [
+                  "tag",
+                  "-a",
+                  prefixedVersion,
+                  "-m",
+                  prefixedVersion,
+                ]),
+            },
+          ]),
+      };
+    })
+  );
+
+  tasks.run();
+})();
