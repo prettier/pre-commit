97489224bb3ce31889860fa1901e3a40cf8edfa7
fisker <lionkay@gmail.com>
2020-08-25 17:15:29 +0800

    Update script

--- scripts/release-version.js
+++ scripts/release-version.js
@@ -3,42 +3,11 @@
 const fs = require("fs");
 const path = require("path");
 const execa = require("execa");
-const fetch = require("node-fetch");
 const Listr = require("listr");
-const prompt = require("enquirer").prompt;
-const yaml = require("js-yaml");
 const prettier = require("prettier");
+const prettierVersion = require("./package.json").devDependencies.prettier;
 
 const ROOT = path.join(__dirname, "..");
-
-const CACHE_FILE = path.join(ROOT, "registry-cache.json");
-const CACHE_EXPIRES = 1 * 60 * 60 * 1000;
-async function getVersions() {
-  try {
-    const stat = fs.statSync(CACHE_FILE);
-    if (new Date() - stat.ctime < CACHE_EXPIRES) {
-      return require(CACHE_FILE);
-    }
-  } catch {}
-
-  const response = await fetch("https://registry.npmjs.org/prettier");
-  const json = await response.json();
-  const versions = Object.keys(json.versions).reverse();
-  fs.writeFileSync(CACHE_FILE, JSON.stringify(versions, undefined, 2));
-  return versions;
-}
-
-async function selectVersions(versions) {
-  const { selected } = await prompt({
-    type: "multiselect",
-    name: "selected",
-    message: "Select version(s) you want create:",
-    choices: versions,
-    initial: versions,
-    limit: 15,
-  });
-  return selected;
-}
 
 let hooks;
 const hooksFile = path.join(ROOT, ".pre-commit-hooks.yaml");
@@ -56,54 +25,42 @@ const git = (command, commandArguments, silence = true) => {
 };
 
 (async () => {
-  const allVersions = await getVersions();
-  let versions = process.argv.slice(2);
-
-  for (const version of versions) {
-    if (!allVersions.includes(version)) {
-      throw new Error(`Version ${version} is not found.`);
-    }
-  }
-
-  if (versions.length === 0) {
-    versions = await selectVersions(allVersions);
-  }
-
-  const tasks = new Listr(
-    versions.map((version) => {
-      const prefixedVersion = `v${version}`;
-      return {
-        title: prefixedVersion,
-        task: () =>
-          new Listr([
-            {
-              title: `Switch to "main" branch`,
-              task: () => git("checkout", ["main"], false),
-            },
-            {
-              title: `Create "${prefixedVersion}" branch`,
-              task: () => git("branch", [prefixedVersion]),
-            },
-            {
-              title: `Switch to "${prefixedVersion}" branch`,
-              task: () => git("checkout", [prefixedVersion]),
-            },
-            {
-              title: `Update files`,
-              task: () => updateFiles(version),
-            },
-            {
-              title: `Commit files`,
-              task: () => git("commit", ["-am", prefixedVersion]),
-            },
-            {
-              title: `Switch back to "main" branch`,
-              task: () => git("checkout", ["main"], false),
-            },
-          ]),
-      };
-    })
-  );
+  const prefixedVersion = `v${prettierVersion}`;
+  const tasks = new Listr([
+    {
+      title: `Switch to "main" branch`,
+      task: () => git("checkout", ["main"], false),
+    },
+    {
+      title: `Create "${prefixedVersion}" branch`,
+      task: () => git("branch", [prefixedVersion]),
+    },
+    {
+      title: `Switch to "${prefixedVersion}" branch`,
+      task: () => git("checkout", [prefixedVersion]),
+    },
+    {
+      title: `Update files`,
+      task: () => updateFiles(prettierVersion),
+    },
+    {
+      title: `Commit files`,
+      task: () => git("commit", ["-am", prefixedVersion]),
+    },
+    {
+      title: `Push branch`,
+      task: () =>
+        git("push", [
+          "--force",
+          "--repo",
+          "git@github.com:prettier/pre-commit.git",
+        ]),
+    },
+    {
+      title: `Switch back to "main" branch`,
+      task: () => git("checkout", ["main"], false),
+    },
+  ]);
 
   tasks.run();
 })().catch((error) => {
